8a118255f9aae61c7fc60d7b66b5575390b6b3fe
Davide Chicco <davide.chicco@gmail.com>
2018-04-27 16:24:58 -0400

    Added files
    Added files for random forest classification and PNN

---
+++ pnn_mesothelioma_initial.py
@@ -0,0 +1,81 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.metrics import matthews_corrcoef
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.metrics import accuracy_score
+
+from neupy.algorithms import PNN
+import time
+start = time.time()
+
+fileName="../data/MesotheliomaDataSet_DicleUniversity_NORMALIZED.csv"
+TARGET_COLUMN=35
+
+# fileName="./MesotheliomaDataSet_DicleUniversity_NORMALIZED_withoutDiagnosisMethod.csv"
+# TARGET_COLUMN=34
+
+
+
+from numpy import genfromtxt
+mesothelioma_data = genfromtxt(fileName, delimiter=',', skip_header=1, usecols=(range(0, TARGET_COLUMN-1)))
+#print(mesothelioma_data)
+mesothelioma_target = genfromtxt(fileName, delimiter=',', skip_header=1, usecols=(TARGET_COLUMN-1))
+#print(mesothelioma_target)
+
+#print(data)
+
+#dataset = datasets.load_iris()
+#data = dataset.data
+#target = dataset.target
+
+#mesothelioma_data = data
+#mesothelioma_target = target
+
+# kfold_number = 5 # ORIGINAL
+
+kfold_number = 5
+skfold = StratifiedKFold(mesothelioma_target, kfold_number, shuffle=True)
+avarage_result = 0
+
+accu_sum = 0
+mcc_sum = 0
+
+print("> Start classify mesothelioma dataset")
+
+for i, (train, test) in enumerate(skfold, start=1):
+    x_train, x_test = mesothelioma_data[train], mesothelioma_data[test]
+    y_train, y_test = mesothelioma_target[train], mesothelioma_target[test]
+
+    pnn_network = PNN(std=0.1, step=0.2,  verbose=True) # BEST
+    #pnn_network = PNN(std=0.1, step=0.2,  verbose=True, batch_size=20)
+    
+    # pnn_network.train(x_train, y_train)
+    # predictions = pnn_network.predict(x_test)
+    pnn_network.train(mesothelioma_data[train], mesothelioma_target[train])
+    predictions = pnn_network.predict(mesothelioma_data[test])
+    
+    # print(predictions)
+    #print(mesothelioma_target[test])
+    
+    mcc = matthews_corrcoef(mesothelioma_target[test], predictions)
+    print("The Matthews correlation coefficient is %.2f" % mcc)
+    
+    accu = accuracy_score(mesothelioma_target[test], predictions)
+    print("The accuracy is %.2f" % accu)
+    accu_sum = accu_sum + accu
+    mcc_sum = mcc_sum + mcc
+
+    print("kfold #{:<2}: Guessed {} out of {}".format(
+        i, np.sum(predictions == mesothelioma_target[test]), test.size
+    ))
+
+mcc_average = mcc_sum/kfold_number
+accu_average = accu_sum/kfold_number
+
+print("\n>>> The average MCC is %.3f" % mcc_average)
+print(">>> The average accuracy is %.3f\n" % accu_average)
+
+end = time.time()
+hours, rem = divmod(end-start, 3600)
+minutes, seconds = divmod(rem, 60)
+print("{:0>2} hours {:0>2} minutes {:05.2f} seconds".format(int(hours),int(minutes),seconds))
---
+++ random_forests_class.r
@@ -0,0 +1,65 @@
+setwd(".")
+options(stringsAsFactors = FALSE)
+# library("clusterSim")
+library("PRROC")
+library("e1071")
+library("randomForest");
+
+source("./confusion_matrix_rates.r")
+
+threshold <- 0.5
+
+prc_data_norm <- read.csv(file="../../Mesothelioma_project/data/MesotheliomaDataSet_original_COL_NORM.csv",head=TRUE,sep=",",stringsAsFactors=FALSE)
+
+prc_data_norm$"diagnosis.method" <- NULL
+
+prc_data_norm <- prc_data_norm[sample(nrow(prc_data_norm)),] # shuffle the rows
+
+target_index <- dim(prc_data_norm)[2]
+
+
+
+# the training set is the first 60% of the whole dataset
+training_set_first_index <- 1 # NEW
+training_set_last_index <- round(dim(prc_data_norm)[1]*60/100) # NEW
+
+# the test set is the last 40% of the whole dataset
+test_set_first_index <- training_set_last_index+1 # NEW
+test_set_last_index <- dim(prc_data_norm)[1] # NEW
+
+cat("[Creating the subsets for the values]\n")
+prc_data_train <- prc_data_norm[training_set_first_index:training_set_last_index, 1:(target_index)] # NEW
+prc_data_test <- prc_data_norm[test_set_first_index:test_set_last_index, 1:(target_index)] # NEW
+
+cat("[Creating the subsets for the labels \"1\"-\"0\"]\n")
+prc_data_train_labels <- prc_data_norm[training_set_first_index:training_set_last_index, target_index] # NEW
+prc_data_test_labels <- prc_data_norm[test_set_first_index:test_set_last_index, target_index]   # NEW
+
+
+library(class)
+library(gmodels)
+
+rf_new <- randomForest(class.of.diagnosis ~ ., data=prc_data_train, importance=TRUE, proximity=TRUE)
+prc_data_test_PRED <- predict(rf_new, prc_data_test, type="response")
+
+prc_data_test_PRED_binary <- as.numeric(prc_data_test_PRED)
+
+prc_data_test_PRED_binary[prc_data_test_PRED_binary>=threshold]=1
+prc_data_test_PRED_binary[prc_data_test_PRED_binary<threshold]=0
+
+print(prc_data_test$class.of.diagnosis)
+print(prc_data_test_PRED_binary)
+# prc_data_test_PRED_binary
+
+fg_test <- prc_data_test_PRED[prc_data_test_labels==1]
+bg_test <- prc_data_test_PRED[prc_data_test_labels==0]
+pr_curve_test <- pr.curve(scores.class0 = fg_test, scores.class1 = bg_test, curve = F)
+#plot(pr_curve_test)
+# print(pr_curve_test)
+
+mcc_outcome <- mcc(prc_data_test_labels, prc_data_test_PRED_binary)
+
+confusion_matrix_rates(prc_data_test_labels, prc_data_test_PRED_binary)
+
+
+
