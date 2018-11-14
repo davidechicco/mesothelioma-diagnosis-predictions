118473c165324b493beb81d40686c123332ac7f9
Davide Chicco <davide.chicco@gmail.com>
2018-11-14 13:56:18 -0500

    edited PNN python2

--- pnn_mesothelioma_initial.py
+++ pnn_mesothelioma_initial.py
@@ -1,15 +1,29 @@
+from __future__ import division
 import numpy as np
 from sklearn import datasets
 from sklearn.metrics import matthews_corrcoef
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 
 from neupy.algorithms import PNN
 import time
 start = time.time()
 
-fileName="../data/MesotheliomaDataSet_DicleUniversity_NORMALIZED.csv"
-TARGET_COLUMN=35
+
+def median(lst):
+    n = len(lst)
+    if n < 1:
+            return None
+    if n % 2 == 1:
+            return sorted(lst)[n//2]
+    else:
+            return sum(sorted(lst)[n//2-1:n//2+1])/2.0
+
+fileName="./Mesothelioma_data_set_COL_NORM.csv"
+TARGET_COLUMN=34
 
 # fileName="./MesotheliomaDataSet_DicleUniversity_NORMALIZED_withoutDiagnosisMethod.csv"
 # TARGET_COLUMN=34
@@ -33,12 +47,21 @@ mesothelioma_target = genfromtxt(fileName, delimiter=',', skip_header=1, usecols
 
 # kfold_number = 5 # ORIGINAL
 
-kfold_number = 5
+vectMCC = []
+vectAcc = []
+vectSpec = []
+vectSens = []
+vectF1 = []
+
+kfold_number = 10
 skfold = StratifiedKFold(mesothelioma_target, kfold_number, shuffle=True)
 avarage_result = 0
 
 accu_sum = 0
 mcc_sum = 0
+specificity_sum = 0
+thisF1_sum = 0
+sensitivity_sum = 0
 
 print("> Start classify mesothelioma dataset")
 
@@ -56,14 +79,39 @@ for i, (train, test) in enumerate(skfold, start=1):
     
     # print(predictions)
     #print(mesothelioma_target[test])
+      
+    tn, fp, fn, tp = confusion_matrix(mesothelioma_target[test], predictions).ravel()
+    print("tn, fp, fn, tp")
+    print(tn, fp, fn, tp, )
     
     mcc = matthews_corrcoef(mesothelioma_target[test], predictions)
     print("The Matthews correlation coefficient is %.2f" % mcc)
+   
     
     accu = accuracy_score(mesothelioma_target[test], predictions)
     print("The accuracy is %.2f" % accu)
+    
+    sensitivity = recall_score(mesothelioma_target[test], predictions)
+    print("The sensitivity is %.2f" % sensitivity)
+    
+    thisF1 = f1_score(mesothelioma_target[test], predictions)
+    print("The F1 score is %.2f" % thisF1)
+    
+    specificity = tn / (tn + fp)
+    print("The specificity is %.2f" % specificity)
+
+    specificity_sum = specificity_sum + specificity
+    thisF1_sum = thisF1_sum + thisF1
+    sensitivity_sum = sensitivity_sum + sensitivity
     accu_sum = accu_sum + accu
     mcc_sum = mcc_sum + mcc
+    
+    vectMCC.append(mcc)
+    vectSens.append(sensitivity)
+    vectSpec.append(specificity)
+    vectAcc.append(accu)
+    vectF1.append(thisF1)
+
 
     print("kfold #{:<2}: Guessed {} out of {}".format(
         i, np.sum(predictions == mesothelioma_target[test]), test.size
@@ -71,9 +119,26 @@ for i, (train, test) in enumerate(skfold, start=1):
 
 mcc_average = mcc_sum/kfold_number
 accu_average = accu_sum/kfold_number
+specificity_average = specificity_sum/kfold_number
+thisF1_average = thisF1_sum/kfold_number
+sensitivity_average = sensitivity_sum/kfold_number
 
 print("\n>>> The average MCC is %.3f" % mcc_average)
-print(">>> The average accuracy is %.3f\n" % accu_average)
+print(">>> The average accuracy is %.3f" % accu_average)
+print("\n>>> The average specificity is %.3f" % specificity_average)
+print(">>> The average sensitivity is %.3f" % sensitivity_average)
+print(">>> The average F1 score is %.3f\n" % thisF1_average)
+
+print("\n>>> The median MCC is ")
+print(median(vectMCC))
+print("\n>>> The median sensitivity is ")
+print(median(vectSens))
+print("\n>>> The median specificity is ")
+print(median(vectSpec))
+print("\n>>> The median accuracy is ")
+print(median(vectAcc))
+print("\n>>> The median F1 score is ")
+print(median(vectF1))
 
 end = time.time()
 hours, rem = divmod(end-start, 3600)
